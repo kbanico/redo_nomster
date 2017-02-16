672a958deadb2c741a84164cf946f955d8bbfc13
kbanico <code4uguam@gmail.com>
2017-02-16 04:39:59 +0000

    add font awesome gem

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,25 @@
 class PlacesController < ApplicationController
   def index
-    @places = Place.all
+    @places = Place.order('created_at DESC').page(params[:page])
   end
+
+  def new
+    @place = Place.new
+  end
+
+  def create
+    Place.create(place_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def set_place
+    @place = Place.find(params[:id])
+  end
+
+  def place_params
+    params.require(:place).permit(:name, :description, :address)
+  end
+
 end
