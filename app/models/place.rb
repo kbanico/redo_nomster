95e39cca398878d16635f9f88b6e71fcbb45b138
kbanico <code4uguam@gmail.com>
2017-02-17 06:45:55 +0000

    add client side validation gem to master

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,5 +1,7 @@
 class Place < ApplicationRecord
   belongs_to :user
   self.per_page = 10
-  validates :name, presence: true
+  validates :name, presence: true, length: {minimum: 3, maximum: 30}
+  validates :description, presence: true, length: {minimum: 10, maximum: 300}
+  validates :address, presence: true, length: {minimum: 5, maximum: 100}
 end
