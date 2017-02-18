a00d83b00b1dcaa3c4a67a328fc21ad9e87ada0d
kbanico <code4uguam@gmail.com>
2017-02-18 08:28:37 +0000

    add category model

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {registrations: 'registrations'}
   root "places#index"
   resources :places
+  resources :categories, except: [:destroy]
 end
