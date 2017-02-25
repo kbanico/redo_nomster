7ca2a50d5c2fd398588c96751a191f4954948314
kbanico <code4uguam@gmail.com>
2017-02-25 03:19:16 +0000

    carrierwave implemented

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,7 +1,7 @@
 class PhotosController < ApplicationController
   def create
     @place = Place.find(params[:place_id])
-    @place.photos.create(photo_params)
+    @place.photos.create(photo_params.merge(user: current_user))
     redirect_to place_path(@place)
 
 
@@ -13,6 +13,8 @@ class PhotosController < ApplicationController
     params.require(:photo).permit(:caption, :picture)
   end
 end
+
+
 
 
 
