673325d849cf8c6d48c7f1e0e5f55849b3ff9bf0
kbanico <code4uguam@gmail.com>
2017-02-27 13:41:36 +0000

    ajax for photo_comments not working

--- app/controllers/comment_to_photos_controller.rb
+++ app/controllers/comment_to_photos_controller.rb
@@ -1,22 +1,18 @@
 class CommentToPhotosController < ApplicationController
   before_action :authenticate_user!, only: [:create, :destroy]
   def create
-    # @place = Place.find_by_id(params[:photo_id])
-
-    # @place.photos.comment_to_photos.create(comment_to_photo_params.merge(user: current_user).merge(photo: @place.photos.id))
-
     @photo = Photo.find(params[:photo_id])
-    # @photo.user_id = current_user.id
-    # @photo.place_id = @photo.place.id
-
-
-
     @photo.comment_to_photos.create(comment_to_photo_params.merge(user_id:current_user.id, place_id: @photo.place.id))
-    if @photo.valid?
-      flash[:success] = "Added Your comment"
-      redirect_to(:back)#place_path(@photo.place.id)
+    if @photo.save
+      respond_to do |format|
+        format.html {redirect_to(:back)}
+        format.js
+      end
+      # flash[:success] = "Added Your comment"
+      # redirect_to(:back)#place_path(@photo.place.id)
     else
-      flash[:danger] = "Was not able to add comment"
+      flash[:danger] = "Check the comment form, something went
+wrong."
       redirect_to root_path
     end
   end
