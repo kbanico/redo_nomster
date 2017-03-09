b01b67abd575587162359adbcb85687c9448f4f0
kbanico <code4uguam@gmail.com>
2017-03-09 07:42:38 +0000

    add before action to photos_to_controller

--- app/controllers/comment_to_photos_controller.rb
+++ app/controllers/comment_to_photos_controller.rb
@@ -1,5 +1,5 @@
 class CommentToPhotosController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, only: [:create, :destroy]
   before_action :find_photo
 
   def index
