d51826c1ca4688bb70680bc8fa90bfd1c5243ca5
kbanico <code4uguam@gmail.com>
2017-03-14 07:11:35 +0000

    redid comments to photos and just make it loop through all comments

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -19,6 +19,39 @@ class MessagesController < ApplicationController
     end
   end
 
+  def destroy
+    @message = Message.find(params[:id])
+    # if current_user != @message.user
+    #   return render text: 'Not Allowed', status: :forbidden
+
+    # else
+    #   @message.destroy
+    #   ActionCable.server.broadcast 'room_channel',
+    #                                    content: @message.content,
+    #                                    username: @message.user.username,
+    #                                    time: time_ago_in_words(@message.created_at)
+    #   # flash[:success] = "Message was removed"
+    #   # redirect_to root_path
+
+    # end
+
+     if current_user == @message.user
+      @message.destroy
+      respond_to do |format|
+        #flash[:info] = "Comment Deleted"
+        format.html{redirect_to(:back)}
+        format.js
+      end
+    else
+      flash[:warning] = "You are not authorized to do that"
+      redirect_to root_paths
+
+
+      end
+  end
+
+
+
   private
     def message_params
         params.require(:message).permit(:content)
