62a79a7cdf30cee59dfa8c16b681e165e0ffaaa8
kbanico <code4uguam@gmail.com>
2017-03-15 08:38:22 +0000

    gonna add avatar to user

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -5,6 +5,10 @@ class ProfilesController < ApplicationController
     @photos = User.find_by(username: params[:username]).photos.order("created_at DESC")
     @user = User.find_by(username: params[:username])
   end
+
+  def edit
+    @user = User.find_by(user_name: params[:username])
+  end
 end
 
 
