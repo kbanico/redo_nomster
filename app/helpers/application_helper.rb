31d648fc07b1c878ecbc48ab2c3989cb1300e456
kbanico <code4uguam@gmail.com>
2017-03-18 11:21:57 +0000

    avatar showing on chat

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,9 +2,9 @@ module ApplicationHelper
 
   def profile_avatar_select(user)
 
-    if @user.avatar.present?
+    if user.avatar.present?
 
-      return image_tag @user.avatar, id: 'image-preview', class: "img-fluid rounded-circle profile-image"
+      return image_tag user.avatar, id: 'image-preview', class: "img-fluid rounded-circle profile-image"
     else
       return image_tag 'user.png', id:'image-preview', class: "img-fluid rounded-circle profile-image"
     end
