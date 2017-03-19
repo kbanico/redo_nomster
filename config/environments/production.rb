5dc13422129efa514b903a9022a3603778804889
kbanico <code4uguam@gmail.com>
2017-03-19 01:56:21 +0000

    action cable set up

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -39,7 +39,7 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
    config.action_cable.url = 'wss://review-app-ken-banico.herokuapp.com/cable'
-   config.action_cable.allowed_request_origins = [ 'https://review-app-ken-banico.herokuapp.com', '/http:\/\/review-app-ken-banico.herokuapp.*/']
+   config.action_cable.allowed_request_origins = [ 'https://review-app-ken-banico.herokuapp.com']
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
